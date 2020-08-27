b9e21e63b790615083aed6e9a0014bfcbc75faf4
Samuel Drysén <samuel.drysen@live.se>
2020-08-27 03:17:06 +0200

    Add project files.

---
+++ SunBot/Program.cs
@@ -0,0 +1,25 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace SunBot
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+            => new Program().MainAsync().GetAwaiter().GetResult();
+        
+
+        public async Task MainAsync()
+        {
+            DiscordSocketClient client;
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}

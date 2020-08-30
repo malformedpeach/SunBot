2660b75fde8b06b129fbcc1cf1b3b3e924b97c23
Samuel Drysén <samuel.drysen@live.se>
2020-08-30 12:33:58 +0200

    Ban command Completed ban command

--- SunBot/Modules/AdminModule.cs
+++ SunBot/Modules/AdminModule.cs
@@ -1,6 +1,8 @@
 ﻿using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using SunBot.Services;
 using System;
 using System.Collections.Generic;
 using System.Reflection.Emit;
@@ -11,29 +13,39 @@ namespace SunBot.Modules
 {
     public class AdminModule : ModuleBase<SocketCommandContext>
     {
+        private Configuration _config;
+        private IServiceProvider _service;
+
+        public AdminModule(IServiceProvider service)
+        {
+            _config = service.GetRequiredService<Configuration>();
+            _service = service;
+        }
+
         [Command("ban")]
         [Alias("banuser")]
         [Summary("Bans a specified user")]
         [RequireUserPermission(GuildPermission.Administrator)]
-        public async Task BanUserAsync([Summary("User to ban")] SocketUser user = null)
+        public async Task BanUserAsync([Summary("User to ban")] SocketUser user = null, [Remainder][Summary("Reason for ban")]string banReason = null)
         {
             if (user == null)
             {
                 var embed = new EmbedBuilder
                 {
                     Title = "Command error",
-                    Description = "Enter a **user** to ban when using this command.",
+                    Description = "Mention a **user** to ban when using this command. \nOptional parameter: **ban reason**",
                     Color = Color.Gold,
                 };
-                embed.AddField("Example", "banuser @user", false);
+                embed.AddField("Example", $"{_config.Bot.Prefix}ban @user **ban reason**\n{_config.Bot.Prefix}banuser @user **ban reason**", false);
 
                 await ReplyAsync(embed: embed.Build());
             }
             else
             {
                 // Doesn't work, figure out why
-                //await Context.Guild.AddBanAsync(user, 0, null);
-                await ReplyAsync($"Banning user: {user.Username}#{user.Discriminator}");
+                // Solution: didn't work because i never authorized the bot to ban users. 
+                await Context.Guild.AddBanAsync(user: user, reason: banReason);
+                await ReplyAsync($"{user} banned for: {banReason ?? "No reason."}");
             }
         }
     }

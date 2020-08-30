ccb03f8aceec0d7431b34c34b08ce506cf29dfc7
Samuel Drysén <samuel.drysen@live.se>
2020-08-30 18:19:15 +0200

    Clear messages added Added a clear messages command that clears a specified amount of messages in the text channel where it's invoked. Doesn't really work though

--- SunBot/Modules/AdminModule.cs
+++ SunBot/Modules/AdminModule.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using SunBot.Services;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,19 +15,17 @@ namespace SunBot.Modules
     public class AdminModule : ModuleBase<SocketCommandContext>
     {
         private Configuration _config;
-        private IServiceProvider _service;
 
         public AdminModule(IServiceProvider service)
         {
             _config = service.GetRequiredService<Configuration>();
-            _service = service;
         }
 
         [Command("ban")]
-        [Alias("banuser")]
         [Summary("Bans a specified user")]
+        [Alias("banuser")]
         [RequireUserPermission(GuildPermission.Administrator)]
-        public async Task BanUserAsync([Summary("User to ban")] SocketUser user = null, [Remainder][Summary("Reason for ban")]string banReason = null)
+        public async Task BanUserAsync([Summary("User to ban")] SocketGuildUser user = null, [Remainder][Summary("Reason for ban")]string banReason = null)
         {
             if (user == null)
             {
@@ -47,6 +46,45 @@ namespace SunBot.Modules
                 await Context.Guild.AddBanAsync(user: user, reason: banReason);
                 await ReplyAsync($"{user} banned for: {banReason ?? "No reason."}");
             }
+        }
+
+        [Command("kick")]
+        [Summary("Kicks a specified user")]
+        [Alias("kickuser")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task KickUserAsync([Summary("User to kick")] SocketGuildUser user = null, [Remainder][Summary("Reason for kick")]string kickReason = null)
+        {
+            if (user == null)
+            {
+                var embed = new EmbedBuilder
+                {
+                    Title = "Command error",
+                    Description = "Mention a **user** to kick when using this command.",
+                    Color = Color.Gold
+                };
+                embed.AddField("Example", $"{_config.Bot.Prefix}kick @user **kick reason**\n{_config.Bot.Prefix}kickuser @user **kick reason**", false);
+
+                await ReplyAsync(embed: embed.Build());
+            }
+            else
+            {
+                await user.KickAsync(kickReason);
+                await ReplyAsync($"{user} kicked for: {kickReason ?? "No reason."}");
+            }
+        }
+
+        [Command("clear")]
+        [Summary("Clears a specified amount of messages from a text channel")]
+        [Alias("purge")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task ClearChannelAsync([Summary("Amount of messages to clear")]int amount)
+        {
+            var messages = await Context.Channel.GetMessagesAsync(amount + 1).FlattenAsync();
+            await (Context.Channel as ITextChannel).DeleteMessagesAsync(messages);
+
+            var purgeMessage = await ReplyAsync($"{amount} messages purged, this message will be deleted in 5 seconds.");
+            await Task.Delay(5000);
+            await purgeMessage.DeleteAsync();
         }
     }
 }
--- SunBot/Modules/InfoModule.cs
+++ SunBot/Modules/InfoModule.cs
@@ -2,12 +2,31 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace SunBot.Modules
 {
     public class InfoModule : ModuleBase<SocketCommandContext>
     {
+        private DiscordSocketClient _client;
+
+        public InfoModule(IServiceProvider service)
+        {
+            _client = service.GetRequiredService<DiscordSocketClient>();
+            _client.UserJoined += AnnounceJoinedUser;
+        }
+
+        public async Task AnnounceJoinedUser(SocketGuildUser user)
+        {
+            // Additional styling and logic to check if bot user joined etc
+            
+            //var channel = user.Guild.DefaultChannel;
+            //await channel.SendMessageAsync($"Welcome! {user.Username}");
+        }
+
         [Command("say")]
         [Summary("Echoes a message.")]
         [Alias("echo")]
@@ -17,7 +36,6 @@ namespace SunBot.Modules
         [Summary("Info on commands.")]
         public async Task HelpAsync()
         {
-
             await ReplyAsync("foobar");
         }
     }

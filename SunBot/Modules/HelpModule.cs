775050979391caaab70a73134ce9743ade8d27a7
Samuel Drysén <samuel.drysen@live.se>
2020-08-30 18:41:46 +0200

    Added HelpModule Module for information on all commands

---
+++ SunBot/Modules/HelpModule.cs
@@ -0,0 +1,37 @@
+﻿using Discord;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using SunBot.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunBot.Modules
+{
+    [Group("help")]
+    public class HelpModule : ModuleBase<SocketCommandContext>
+    {
+        private Configuration _config;
+
+        public HelpModule(IServiceProvider services)
+        {
+            _config = services.GetRequiredService<Configuration>();
+        }
+
+        [Command("say")]
+        [Summary("Info on command \"say\"")]
+        public async Task HelpSayAsync()
+        {
+            var embed = new EmbedBuilder
+            {
+                Title = "Say Command",
+                Description = "Echoes a message.",
+                Color = Color.Gold
+            };
+            embed.AddField("Example", $"`{_config.Bot.Prefix}say Hello world!`");
+
+            await ReplyAsync(embed: embed.Build());
+        }
+    }
+}
--- SunBot/Modules/InfoModule.cs
+++ SunBot/Modules/InfoModule.cs
@@ -6,25 +6,33 @@ using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
+using SunBot.Services;
 
 namespace SunBot.Modules
 {
     public class InfoModule : ModuleBase<SocketCommandContext>
     {
         private DiscordSocketClient _client;
+        private Configuration _config;
 
         public InfoModule(IServiceProvider service)
         {
             _client = service.GetRequiredService<DiscordSocketClient>();
+            _config = service.GetRequiredService<Configuration>();
             _client.UserJoined += AnnounceJoinedUser;
         }
 
         public async Task AnnounceJoinedUser(SocketGuildUser user)
         {
-            // Additional styling and logic to check if bot user joined etc
-            
-            //var channel = user.Guild.DefaultChannel;
-            //await channel.SendMessageAsync($"Welcome! {user.Username}");
+            //var embed = new EmbedBuilder
+            //{
+            //    Title = "Welcome!",
+            //    Description = $"Welcome to {user.Guild.Name}, {user}."
+            //};
+
+            var channel = user.Guild.DefaultChannel;
+
+            await channel.SendMessageAsync($"Welcome to {user.Guild.Name}, {user}");
         }
 
         [Command("say")]
@@ -36,7 +44,20 @@ namespace SunBot.Modules
         [Summary("Info on commands.")]
         public async Task HelpAsync()
         {
-            await ReplyAsync("foobar");
+            var embed = new EmbedBuilder
+            {
+                Title = "Bot Commands",
+                Description = $"The prefix of the bot is `{_config.Bot.Prefix}`",
+                Color = Color.Gold
+            };
+            embed.AddField("General", "`say`, `help`")
+                 .AddField("Admin", "`ban`, `kick`, `clear`")
+                 .WithFooter($"For more information use {_config.Bot.Prefix}help (command)");
+
+
+            await ReplyAsync(embed: embed.Build());
         }
+
+        
     }
 }
